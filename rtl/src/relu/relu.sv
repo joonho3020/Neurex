3aa85aa617e4236e081ef807f5cfd147d94b5f8f
jsl013 <junseo.lee@snu.ac.kr>
2022-04-07 13:51:27 +0900

    add a relu module

---
+++ rtl/src/relu/relu.sv
@@ -0,0 +1,18 @@
+// combinational logic
+module relu #(
+  parameter int unsigned DATA_WIDTH = 32
+) (
+  input en,
+  input [DATA_WIDTH-1:0] in,
+  output logic [DATA_WIDTH-1:0] out
+);
+
+always_comb begin
+  if (en) begin
+    out = $signed(in) > 0 ? $signed(in) : {DATA_WIDTH{1'b0}};
+  end else begin
+    out = {DATA_WIDTH{1'b0}};
+  end
+end
+
+endmodule
---
+++ rtl/src/relu/relu_arr.sv
@@ -0,0 +1,24 @@
+// combinational logic
+module relu_arr #(
+  parameter  int unsigned SYS_COL = 16,
+  parameter  int unsigned DATA_WIDTH = 32
+) (
+  input en,
+  input [DATA_WIDTH-1:0] in[0:SYS_COL-1],
+  output [DATA_WIDTH-1:0] out[0:SYS_COL-1]
+);
+
+genvar i;
+generate
+  for (i = 0; i < SYS_COL; i = i + 1) begin
+    relu #(
+      .DATA_WIDTH(DATA_WIDTH)
+    ) m_relu(
+      .en(en),
+      .in(in[i]),
+      .out(out[i])
+    );
+  end
+endgenerate
+
+endmodule
